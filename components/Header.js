4509f347613d201b92f0bee941c75626a41a187e
matsinet <matsinet@gmail.com>
2021-04-05 17:45:06 -0500

    scc-66: html lit and image import updates

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,7 @@
-export default st => `
-<header>
-  <h1>Example SPA: ${st.heading}</h1>
-</header>
+import html from "html-literal";
+
+export default st => html`
+  <header>
+    <h1>Example SPA: ${st.heading}</h1>
+  </header>
 `;
